6a448525c3466252631878ff4849697ab57aa1ca
朱锦涛 <zhujintao@qmai.com>
2025-10-28 23:43:57 +0800

    chore(config): update .cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -2,16 +2,15 @@
 
 module.exports = {
   types: [
-    { value: 'feat', name: '✨ feat:     新功能' },
-    { value: 'fix', name: '🐛 fix:      修复Bug' },
-    { value: 'docs', name: '📝 docs:     文档修改' },
-    { value: 'style', name: '💄 style:    代码格式（不影响逻辑）' },
-    { value: 'refactor', name: '♻️ refactor: 代码重构' },
-    { value: 'perf', name: '⚡️ perf:     性能优化' },
-    { value: 'test', name: '✅ test:     添加或修改测试' },
-    { value: 'chore', name: '🔧 chore:    构建/依赖/脚本调整' },
-    { value: 'revert', name: '⏪ revert:   回退提交' }
-  ],
+    { value: 'feat',     name: '✨ feat:      新功能' },
+    { value: 'fix',      name: '🐛 fix:       修复Bug' },
+    { value: 'docs',     name: '📝 docs:      文档修改' },
+    { value: 'style',    name: '💄 style:     代码格式（不影响逻辑）' },
+    { value: 'refactor', name: '♻️ refactor:  代码重构' },
+    { value: 'perf',     name: '⚡️ perf:      性能优化' },
+    { value: 'test',     name: '✅ test:      添加或修改测试' },
+    { value: 'chore',    name: '🔧 chore:     构建/依赖/脚本调整' },
+    { value: 'revert',   name: '⏪ revert:    回退提交' }  ],
   scopes: [
     { name: 'ui' },
     { name: 'api' },
@@ -20,9 +19,9 @@ module.exports = {
     { name: 'docs' }
   ],
   messages: {
-    type: '请选择提交类型（带表情）：',
-    scope: '请输入影响范围（可选，比如模块名）：',
-    subject: '请输入简短描述（建议一句话）：',
+    type: '请选择提交类型：',
+    scope: '请输入影响范围（可选）：',
+    subject: '请输入简短描述：',
     body: '请输入详细说明（可选）：',
     footer: '请输入要关闭的 Issue（可选）：',
     confirmCommit: '是否确认提交以上信息？（回车确认，n取消，e编辑）'
