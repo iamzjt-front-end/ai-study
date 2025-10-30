3f836b12481ccaccbe256ced30e3a5523eb0e5a9
朱锦涛 <zhujintao@qmai.cn>
2025-10-30 16:53:34 +0800

    feat: 占位符

--- 02_Python运算符/代码/01_占位符.py
+++ 02_Python运算符/代码/01_占位符.py
@@ -3,7 +3,7 @@ name = "杰伦"
 age = 45
 salary = 1.4567
 
-print("大家好，我是杰伦，我今年45，我的年薪1.4567亿")
+# print("大家好，我是杰伦，我今年45，我的年薪1.4567亿")
 
 
 # 占位符：（建议掌握）
@@ -11,9 +11,15 @@ print("大家好，我是杰伦，我今年45，我的年薪1.4567亿")
 #   %d : 整数
 #   %f : 小数   %.4f表示保留4位小数，四舍五入
 #   %% : 百分号
+print("大家好，我是%s，我今年%d，我的年薪%.4f亿" % (name, age, salary))
+num = 3.1415926
 
 
 # 花括号占位符.format() （了解）
+print("大家好，我是{}，我今年{}，我的年薪{}亿".format(name, age, salary))
+
+print("我叫{0}，今年{1}岁。{0}喜欢Python。".format(name, age))
+print("我叫{name}，今年{age}岁。".format(name="王五", age=22))
 
 
 # 练习：
@@ -27,3 +33,18 @@ name = input("姓名:")
 age = int(input("年龄:"))
 height = float(input("身高："))
 weight = float(input("体重："))
+print(
+    "大家好，我是%s, 今年%d岁，我身高是%dcm，体重是%.1fkg" % (name, age, height, weight)
+)
+print(
+    "大家好，我是{}, 今年{}岁，我身高是{}cm，体重是{}kg".format(
+        name, age, height, weight
+    )
+)
+print(
+    "大家好，我是{name}, 今年{age}岁，我身高是{height}cm，体重是{weight}kg".format(
+        name="Jack", age="25", height="177.5", weight="75.2"
+    )
+)
+
+print(f"我叫{name}，今年{age}岁")
